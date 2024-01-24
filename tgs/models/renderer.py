c821cb1a65eb50d0adbba879150da2d34a0b1dbb
JaeSungPak <70958612+JaeSungPak@users.noreply.github.com>
2024-01-24 17:56:35 +0900

    Update renderer.py

--- tgs/models/renderer.py
+++ tgs/models/renderer.py
@@ -108,6 +108,7 @@ class GaussianModel(NamedTuple):
     def save_ply(self, path):
         
         xyz = self.xyz.detach().cpu().numpy()
+        xyz_ = self.xyz.detach().cpu().numpy()
         normals = np.zeros_like(xyz)
         features_dc = self.shs[:, :1]
         features_rest = self.shs[:, 1:]
@@ -117,6 +118,9 @@ class GaussianModel(NamedTuple):
         scale = np.log(self.scaling.detach().cpu().numpy())
         rotation = self.rotation.detach().cpu().numpy()
 
+        xyz[:, 0] = xyz_[:, 1]
+        xyz[:, 1] = xyz_[:, 2]
+        xyz[:, 2] = xyz_[:, 0]
         xyz[:, :] *= 1.3
 
         import pdb; pdb.set_trace()
